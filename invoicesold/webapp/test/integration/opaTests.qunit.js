03d64fc842521c1ad8253b8d7d84ab3fcbabf6ef
Roman-Campillo <tumacat@gmail.com>
2022-10-08 14:07:26 +0000

    05 new

--- invoicesold/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["Logaligroup/invoices/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- invoicesold/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"Logaligroup/invoices/test/unit/controller/App.controller"
-], function () {
-	"use strict";
-});

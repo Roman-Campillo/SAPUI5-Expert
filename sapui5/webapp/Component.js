9c2ed0c5a4af9196d8cf3a46d53e0ea327c23f5d
Roman-Campillo <tumacat@gmail.com>
2022-10-13 17:38:13 +0000

    05 new

--- sapui5/webapp/Component.js
+++ sapui5/webapp/Component.js
@@ -1,9 +1,10 @@
 sap.ui.define([
     'sap/ui/core/UIComponent',
     'logaligroup/sapui5/model/models',
-    'sap/ui/model/resource/ResourceModel'
+    'sap/ui/model/resource/ResourceModel',
+    './controller/HelloDialog'
 ],
-    function (UIComponent, models, ResourceModel) {
+    function (UIComponent, models, ResourceModel, HelloDialog) {
         'use strict';
 
         return UIComponent.extend("logaligroup.sapui5.Component", {
@@ -18,13 +19,31 @@ sap.ui.define([
             },
 
         init: function () {
-
+                // call the base component's init function
                 UIComponent.prototype.init.apply(this, arguments);
 
+                //enable routing
+                //this.getRouter().initialize();
+
+                //set the device model
+                //this.setModel(models.createDeviceModel(), "device");
+
+                //set data model on the view
                 this.setModel(models.createRecipient());
 
+                //
+                this._helloDialog = new HelloDialog(this.getRootControl());
+
+                //
                 let i18nModel = new ResourceModel({ bundleName: "logaligroup.sapui5.i18n.i18n" });
                 this.setModel(i18nModel, "i18n")
+            },
+            exit: function() {
+                this._helloDialog.destroy();
+                delete this._helloDialog;
+            },
+            onOpenDialog: function() {
+                this._helloDialog.open();
             }
         });
     });
--- sapui5/webapp/controller/Main.controller.js
+++ sapui5/webapp/controller/Main.controller.js
@@ -1,30 +1,22 @@
 sap.ui.define([
-    'sap/ui/core/mvc/Controller',
-    'sap/m/MessageToast'
+    'sap/ui/core/mvc/Controller'
 
 ],
     /**
      * 
      * @param {typeof sap.ui.core.mvc.Controller} Controller 
-     * @param {typeof sap.m.MessageToast} MessageToast
      * 
      */
-    function (Controller, MessageToast) {
+    function (Controller) {
         'use strict';
 
         return Controller.extend("logaligroup.sapui5.controller.Main", {
 
             onInit: function () {
             },
-            
-            onShowhello: function () {
 
-                var oBundle = this.getView().getModel("i18n").getResourceBundle();
-                var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-                var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-                MessageToast.show(sMsg);
+            onOpenDialogHeader: function() {
+                this.getOwnerComponent().onOpenDialog();
             }
-
-
         });
     });
